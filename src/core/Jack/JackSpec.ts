3efa2071859627cdc1d2ff7da781094b5e7075ac
Daylen Smith <daylencsmith@gmail.com>
2025-12-18 19:06:38 -0800

    add regex spec for jack tokenization

---
+++ src/core/Jack/JackSpec.ts
@@ -0,0 +1,12 @@
+import { TokenType } from "../Token";
+
+export const JackSpec: [RegExp, TokenType][] = [
+  [/^\/\/.*/, TokenType.SKIP],
+  [/^\/\*[\s\S]*?\*\//, TokenType.SKIP],
+  [/^\s+/, TokenType.SKIP],
+  [/^(class|constructor|method|function|int|boolean|char|void|var|static|field|let|do|if|else|while|return|true|false|null|this)\b/, TokenType.KEYWORD],
+  [/^[{}()\[\].,;+\-*/&|<>=~]/, TokenType.SYMBOL],
+  [/^\d+/, TokenType.INT],
+  [/^"[^"]*"/, TokenType.STRING],
+  [/^[a-zA-Z_][a-zA-Z0-9_]*/, TokenType.IDENTIFIER],
+];
