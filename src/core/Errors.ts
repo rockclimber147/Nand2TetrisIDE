c5916928ff689bbebf6436522b6fcf1e8628c887
Daylen Smith <daylencsmith@gmail.com>
2025-12-18 20:18:32 -0800

    Add validator and errors

---
+++ src/core/Errors.ts
@@ -0,0 +1,19 @@
+import { type Token } from './Token';
+
+export class CompilerError extends Error {
+  public readonly line: number;
+  public readonly column: number;
+  public readonly lexeme: string;
+
+  constructor(public readonly token: Token, message: string) {
+    const fullMessage = `[Line ${token.line}:${token.column}] ${message} (Found: '${token.lexeme}')`;
+    super(fullMessage);
+    
+    this.name = 'CompilerError';
+    this.line = token.line;
+    this.column = token.column;
+    this.lexeme = token.lexeme;
+
+    Object.setPrototypeOf(this, CompilerError.prototype);
+  }
+}
