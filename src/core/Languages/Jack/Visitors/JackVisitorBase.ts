ea366c26bbb3ef917d63149d8a52160e4ce73d04
Daylen Smith <daylencsmith@gmail.com>
2025-12-19 14:11:00 -0800

    format

--- src/core/Languages/Jack/Visitors/JackVisitorBase.ts
+++ src/core/Languages/Jack/Visitors/JackVisitorBase.ts
@@ -5,8 +5,10 @@ import * as JackAST from '../AST';
 export abstract class JackVisitorTopLevel<T> extends VisitorBase<T> {
   public override visit(node: ASTNode): T {
     switch (node.kind) {
-      case ASTNodeKind.CLASS:      return this.visitClass(node as JackAST.JackClassNode);
-      case ASTNodeKind.SUBROUTINE: return this.visitSubroutine(node as JackAST.JackSubroutineNode);
+      case ASTNodeKind.CLASS:
+        return this.visitClass(node as JackAST.JackClassNode);
+      case ASTNodeKind.SUBROUTINE:
+        return this.visitSubroutine(node as JackAST.JackSubroutineNode);
       default:
         throw new Error(`JackVisitor does not support node kind: ${ASTNodeKind[node.kind]}`);
     }
@@ -19,13 +21,15 @@ export abstract class JackVisitorTopLevel<T> extends VisitorBase<T> {
 export abstract class JackVisitorAll<T> extends JackVisitorTopLevel<T> {
   public override visit(node: ASTNode): T {
     switch (node.kind) {
-      case ASTNodeKind.STATEMENT:  return this.visitStatement(node as JackAST.JackStatementNode);
-      case ASTNodeKind.EXPRESSION: 
-      case ASTNodeKind.TERM:        return this.visitExpression(node as JackAST.JackExpressionNode);
+      case ASTNodeKind.STATEMENT:
+        return this.visitStatement(node as JackAST.JackStatementNode);
+      case ASTNodeKind.EXPRESSION:
+      case ASTNodeKind.TERM:
+        return this.visitExpression(node as JackAST.JackExpressionNode);
       case ASTNodeKind.CLASS:
       case ASTNodeKind.SUBROUTINE:
         return super.visit(node);
-        
+
       default:
         throw new Error(`JackVisitorAll does not support node kind: ${ASTNodeKind[node.kind]}`);
     }
