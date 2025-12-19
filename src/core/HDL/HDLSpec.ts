c106259a2c7df3addf87401c7a171b7bb780ca36
Daylen Smith <daylencsmith@gmail.com>
2025-12-18 19:23:59 -0800

    add HDL spec

--- src/core/HDL/HDLSpec.ts
+++ src/core/HDL/HDLSpec.ts
@@ -1,2 +1,12 @@
 import { TokenType } from "../Token";
 
+export const HDLSpec: [RegExp, TokenType][] = [
+  [/^\s+/, TokenType.SKIP],
+  [/^\/\/.*/, TokenType.SKIP],
+  [/^\/\*[\s\S]*?\*\//, TokenType.SKIP],
+  [/^(CHIP|IN|OUT|PARTS|BUILTIN|CLOCKED)\b/, TokenType.KEYWORD],
+  [/^(true|false)\b/, TokenType.KEYWORD],
+  [/^[{}()\[\].,;=]/, TokenType.SYMBOL],
+  [/^\d+/, TokenType.INT],
+  [/^[a-zA-Z_][a-zA-Z0-9_]*/, TokenType.IDENTIFIER],
+];
