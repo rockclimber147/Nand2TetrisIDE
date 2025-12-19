d75d6925cc96ec147e61e3ce14fb8d5819157c19
Daylen Smith <daylencsmith@gmail.com>
2025-12-18 19:06:25 -0800

    Add token and regex based tokenizer

---
+++ src/core/Token.ts
@@ -0,0 +1,17 @@
+export enum TokenType {
+  KEYWORD = 'KEYWORD',
+  SYMBOL = 'SYMBOL',
+  IDENTIFIER = 'IDENTIFIER',
+  INT = 'INT',
+  STRING = 'STRING',
+  NEWLINE = 'NEWLINE',
+  SKIP = 'SKIP',
+  EOF = 'EOF'
+}
+
+export interface Token {
+  type: TokenType;
+  lexeme: string;
+  line: number;
+  column: number;
+}
