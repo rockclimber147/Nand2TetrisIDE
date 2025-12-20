be3b114cacf323e0d197af859511392dca39ce57
rockclimber147 <daylencsmith@gmail.com>
2025-12-19 16:46:27 -0800

    test stubs

--- __test__/Jack/Visitors/SymbolTable/SymbolTable-test.ts
+++ __test__/Jack/Visitors/SymbolTable/SymbolTable-test.ts
@@ -1,1 +1,7 @@
 import { describe, expect, test } from 'vitest';
+import { GlobalSymbolTable } from "../../../../src/core/Languages/Jack/Visitors/SymbolTableVisitor/SymbolTable";
+
+describe("SymbolTable has what's added to it", () => {
+    const table = new GlobalSymbolTable();
+    
+});
