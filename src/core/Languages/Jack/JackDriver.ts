2d2289b0327bd93630b7e3529d4351a3e930b0d3
Daylen Smith <daylencsmith@gmail.com>
2025-12-22 11:51:07 -0800

    allow for parser errors

--- src/core/Languages/Jack/JackDriver.ts
+++ src/core/Languages/Jack/JackDriver.ts
@@ -7,38 +7,50 @@ import { GlobalSymbolTable } from './Visitors/SymbolTableVisitor/SymbolTable';
 import { SymbolTableVisitor } from './Visitors/SymbolTableVisitor/SymbolTableVisitor';
 import { JackSemanticVisitor } from './Visitors/SemanticVisitor/SemanticVisitor';
 import { SymbolTableBuiltinBuilder } from './Visitors/SymbolTableVisitor/SymbolTableBuiltInBuilder';
+import { type JackClassNode } from './AST';
 
 export class JackDriver extends LanguageDriver {
   compileProject(files: Record<string, string>): Record<string, CompilerError[]> {
     const fileNames = Object.keys(files);
-    const sources = Object.values(files);
     const projectErrors: Record<string, CompilerError[]> = {};
+    const asts: (JackClassNode | null)[] = [];
 
     fileNames.forEach((name) => (projectErrors[name] = []));
 
-    try {
-      const asts = sources.map((source) => {
+    fileNames.forEach((name) => {
+      try {
+        const source = files[name];
         const tokenizer = new GenericTokenizer(source, JackTokenMatcher);
         const tokens = tokenizer.tokenize();
         const parser = new JackParser(tokens);
-        return parser.parse();
-      });
+        
+        asts.push(parser.parse());
+      } catch (e) {
+        asts.push(null); // Mark as failed
+        if (e instanceof CompilerError) {
+          projectErrors[name].push(e);
+        } else {
+          console.error(`Unexpected error in ${name}:`, e);
+        }
+      }
+    });
 
-      let globalTable = new GlobalSymbolTable();
-      SymbolTableBuiltinBuilder.populate(globalTable);
-      const stVisitor = new SymbolTableVisitor(globalTable);
-      asts.forEach((ast) => {
-        stVisitor.visit(ast);
-      });
+    let globalTable = new GlobalSymbolTable();
+    SymbolTableBuiltinBuilder.populate(globalTable);
+    const stVisitor = new SymbolTableVisitor(globalTable);
+    
+    asts.forEach((ast) => {
+      if (ast) stVisitor.visit(ast);
+    });
 
-      const semanticVisitor = new JackSemanticVisitor(globalTable);
-      asts.forEach((ast, index) => {
-        semanticVisitor.visit(ast);
+    const semanticVisitor = new JackSemanticVisitor(globalTable);
+    asts.forEach((ast, index) => {
+      if (ast) {
         const fileName = fileNames[index];
-        projectErrors[fileName] = semanticVisitor.getErrorsForCurrentPass();
-      });
-    } catch (e) {
-    }
+        semanticVisitor.visit(ast);
+        projectErrors[fileName].push(...semanticVisitor.getErrorsForCurrentPass());
+      }
+    });
 
     return projectErrors;
   }
